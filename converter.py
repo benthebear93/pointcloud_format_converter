6d437de72fcb115c824cfadfc040ee45d19bd1d8
benthebear93 <nswve@naver.com>
2021-10-28 22:19:44 +0900

    format fix

--- converter.py
+++ converter.py
@@ -1,4 +1,3 @@
-
 import sys
 import os
 from PyQt5 import QtWidgets
@@ -7,17 +6,20 @@ from PyQt5.uic import loadUi
 from plyfile import PlyData, PlyElement
 import open3d as o3d
 
+
 class MainWindow(QDialog):
     def __init__(self):
-        super(MainWindow,self).__init__()
-        loadUi("converter.ui",self)
+        super(MainWindow, self).__init__()
+        loadUi("converter.ui", self)
         self.filedir = 0
         self.browse.clicked.connect(self.browsefiles)
         self.convert.clicked.connect(self.pcd_to_ply)
 
     def browsefiles(self):
         self.currt_root = os.getcwd()
-        self.fname = QFileDialog.getOpenFileNames(self, 'Open file', '', 'Pointcloud files (*.pcd *.ply)')
+        self.fname = QFileDialog.getOpenFileNames(
+            self, "Open file", "", "Pointcloud files (*.pcd *.ply)"
+        )
         self.filename.setText(self.fname[0][0])
 
     def pcd_to_ply(self):
@@ -27,26 +29,26 @@ class MainWindow(QDialog):
             print("select ", num_of_select_file, " files")
             select_flag = True
         except:
-            print('Select the file first')
+            print("Select the file first")
 
         if select_flag == True:
             for i in range(num_of_select_file):
-                pc_filename = self.fname[0][i].split('/')[-1][:-4]
-                pc_file_dir = self.fname[0][i][:-(len(pc_filename)+4)]
-            # without open3d"
-                #os.chdir(pc_file_dir)
-                #commad = 'pcl_pcd2ply -format 0 input ' + pc_filename + ".pcd " + pc_filename +".ply"
+                pc_filename = self.fname[0][i].split("/")[-1][:-4]
+                pc_file_dir = self.fname[0][i][: -(len(pc_filename) + 4)]
+                # without open3d"
+                # os.chdir(pc_file_dir)
+                # commad = 'pcl_pcd2ply -format 0 input ' + pc_filename + ".pcd " + pc_filename +".ply"
                 # os.system(commad)
                 print("converting...")
                 pcd = o3d.io.read_point_cloud(self.fname[0][i])
                 print(pcd)
-                o3d.io.write_point_cloud(pc_file_dir+pc_filename+".ply", pcd)
+                o3d.io.write_point_cloud(pc_file_dir + pc_filename + ".ply", pcd)
                 print("convert done")
 
 
-app=QApplication(sys.argv)
-mainwindow=MainWindow()
-widget=QtWidgets.QStackedWidget()
+app = QApplication(sys.argv)
+mainwindow = MainWindow()
+widget = QtWidgets.QStackedWidget()
 widget.addWidget(mainwindow)
 widget.setFixedWidth(400)
 widget.setFixedHeight(300)
